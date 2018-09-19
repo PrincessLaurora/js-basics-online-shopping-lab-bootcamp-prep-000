e69859f5e539ed61428ba496f8614bf48fbabe24
Laura Felce <crepu83@yahoo.it>
2018-09-19 10:00:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function placeOrder(cardNumber) {
   }
  else {
  cart.splice(0, cart.length);  
-   return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
  } 
 
 }
