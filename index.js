1657e325cc7990686c84f805323b2b9c199e9fee
Laura Felce <crepu83@yahoo.it>
2018-09-19 09:43:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,10 +48,11 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-cart.splice(i, 1)}
-return cart}
-
-return 'That item is not in your cart';
+cart.splice(i, 1);
+return cart;
+    }
+}
+return 'That item is not in your cart.';
 }
 
 
